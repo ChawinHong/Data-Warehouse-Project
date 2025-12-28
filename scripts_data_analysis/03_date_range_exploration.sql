10883d9f90cb1fbd85e8b159b6156224997d95e2
Chawin Gump <168575437+ChawinHong@users.noreply.github.com>
2025-12-28 13:31:21 +0700

    Update 03_date_range_exploration.sql

--- scripts_data_analysis/03_date_range_exploration.sql
+++ scripts_data_analysis/03_date_range_exploration.sql
@@ -1,1 +1,18 @@
 
+--3) Date Exploration
+
+-- Find the date of the first and last order
+-- How many years/months of sales are available
+SELECT
+	min(order_date) AS first_order_date,
+	max(order_date) AS last_order_date ,
+	DATEDIFF(month,min(order_date),max(order_date)) AS order_range_month
+FROM [gold].[fact_sales]
+
+-- Find the youngest and the oldest customer
+SELECT
+MIN(birthdate) AS oldest_birthdate,
+MAX(birthdate) AS youngest_birthdate,
+DATEDIFF(YEAR,MIN(birthdate),GETDATE()) AS oldest_age,
+DATEDIFF(YEAR,MAX(birthdate),GETDATE()) AS youngest_age
+FROM gold.dim_customers
