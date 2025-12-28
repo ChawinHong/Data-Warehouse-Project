e47595f0682a637fb644a686680ed787bc8bad40
Chawin Gump <168575437+ChawinHong@users.noreply.github.com>
2025-12-28 13:33:49 +0700

    Add ranking analysis SQL queries for products and customers

--- scripts_data_analysis/06_ranking_analysis.sql
+++ scripts_data_analysis/06_ranking_analysis.sql
@@ -1,1 +1,67 @@
+-- Ranking Analysis
 
+-- Which 5 products generate the highest revenue?
+SELECT TOP 5 
+	p.product_name,
+	SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+	ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue DESC
+
+SELECT *
+FROM (
+SELECT
+	p.product_name,
+	SUM(f.sales_amount) AS total_revenue,
+	Rank() OVER(ORDER BY SUM(f.sales_amount) DESC) AS rank_product
+
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+	ON p.product_key = f.product_key
+GROUP BY p.product_name
+) t
+WHERE rank_product <= 5
+
+
+-- What are the 5 worst-performing products in terms of sales
+
+SELECT TOP 5 
+	p.product_name,
+	SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+	ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue ASC
+
+-- Find the top 10 customers who have generated the highest revenue
+
+SELECT TOP 10
+	c.customer_key
+	, c.first_name
+	, c.last_name
+	, SUM(f.sales_amount) AS total_revenue	
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+	ON f.customer_key = c.customer_key
+GROUP BY c.customer_key
+	, c.first_name
+	, c.last_name
+ORDER BY total_revenue DESC
+
+-- The 3 customers with the fewest orders placed
+
+SELECT TOP 3
+	c.customer_key
+	, c.first_name
+	, c.last_name
+	, COUNT(DISTINCT order_number) AS total_order
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+	ON f.customer_key = c.customer_key
+GROUP BY c.customer_key
+	, c.first_name
+	, c.last_name
+ORDER BY total_order
